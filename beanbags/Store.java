52a73cbdcc39c8d10223c74863231347f5d903cb
Louis Haddrell <louishaddrell@gmail.com>
2016-03-23 21:56:59 +0000

    Store: removed .findBeanBag() overload

--- beanbags/Store.java
+++ beanbags/Store.java
@@ -28,42 +28,9 @@ public class Store implements BeanBagStore
     /*  Methods
     ***************************************************************************/
     /**
-     * Retrieve a BeanBag object from Storage based on ID, ensuring that the
-     * name, manufacturer and free text component are the same, otherwise
-     * create a new BeanBag
+     * Retrieve a BeanBag object from Storage based on ID
      * 
      * @param id    bean bag ID to search for
-     * @param name    bean bag name to search for
-     * @param manufacturer    bean bag manufacturer to search for
-     * @param information    bean bag information to search for
-     */
-    public BeanBag findBeanBag(String id, String name, String manufacturer, String information, int num)
-    throws BeanBagMismatchException{
-        // Iterate BeanBag array searching for the ID
-        for (int i=0; i < this.beanBags.size(); i++){
-            BeanBag bag = (BeanBag) this.beanBags.get(i);
-            if (bag.getId() == id) {
-                if ((bag.getName() == name) && (bag.getManufacturer() == manufacturer) && (bag.getInformation() == information) ) {
-                    // If parameters are all equal, same bag found
-                    bag.setStockCount((bag.getStockCount() + num));
-                }
-                // Otherwise, correct bag not found, so raise exception
-                throw new BeanBagMismatchException();
-            }
-        }
-        
-        // ID not found
-        return null;
-    }
-    
-    /**
-     * Retrieve a BeanBag object from Storage based on ID, ensuring that the
-     * name, manufacturer and free text component are the same, otherwise
-     * create a new BeanBag
-     * 
-     * @param id    bean bag ID to search for
-     * @param name    bean bag name to search for
-     * @param manufacturer    bean bag manufacturer to search for
      */
     public BeanBag findBeanBag(String id) {
         // Iterate BeanBag array searching for the ID
