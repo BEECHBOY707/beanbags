5daf316bea393d01f017166ab3c93c941d24ac0c
Louis Haddrell <louishaddrell@gmail.com>
2016-03-23 15:39:18 +0000

    Added Reservation class

---
+++ beanbags/Reservation.java
@@ -0,0 +1,12 @@
+package beanbags;
+
+public class Reservation
+{
+	private int priceInPence;
+	private int quantity;
+
+	public Reservation(int priceInPence, int quantity) {
+		this.priceInPence = priceInPence;
+		this.quantity = quantity;
+	}
+}
