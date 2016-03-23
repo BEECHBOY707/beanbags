5940d35da93e853da656bb312e242fe97acf1030
Louis Haddrell <louishaddrell@gmail.com>
2016-03-23 15:35:31 +0000

    Added Sale class

---
+++ beanbags/Sale.java
@@ -0,0 +1,12 @@
+package beanbags;
+
+public class Sale
+{
+    private int priceInPence;
+    private int quantity;
+
+    public Sale(int priceInPence, int quantity) {
+        this.priceInPence = priceInPence;
+        this.quantity = quantity;
+    }
+}
