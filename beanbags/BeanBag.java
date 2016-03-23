bb41a0894bc9be29df5cbd67e6f1cce02de35bab
Louis Haddrell <louishaddrell@gmail.com>
2016-03-23 09:59:13 +0000

    BeanBag.java: Getters and setters

--- beanbags/BeanBag.java
+++ beanbags/BeanBag.java
@@ -14,8 +14,8 @@ public class BeanBag
     private static int reservationIdCreationCounter;
 
     private byte month;
-    private int bagStockCount;
     private int priceInPence;
+    private int stockCount;
     private short year;
     private String id;
     private String information;
@@ -30,7 +30,7 @@ public class BeanBag
     public BeanBag(int num, String manufacturer, String name, String id,
                    short year, byte month, String information) {
 
-        this.bagStockCount = num;
+        this.stockCount = num;
         this.manufacturer = manufacturer;
         this.name = name;
         this.id = id;
@@ -43,28 +43,70 @@ public class BeanBag
         BeanBag.reservationIdCreationCounter = 0;
     }
 
-    public String getManufacturer() {
-        return this.manufacturer;
-    }
-    
-    public String getName() {
-        return this.name;
-    }
-    
-    public String getid() {
-        return this.id;
-    }
-    
-    public short getYear() {
-        return this.year;
-    }
-    
+    /*  Getters and Setters
+    ***************************************************************************/
     public short getMonth() {
         return this.month;
     }
-    
+
+    public void setMonth(short value) {
+        this.month = value;
+    }
+
+    public int getPrice() {
+        return this.priceInPence;
+    }
+
+    public void setPrice(int value) {
+        this.priceInPence = value;
+    }
+
+    public int getStockCount() {
+        return this.stockCount;
+    }
+
+    public void setStockCount(int value) {
+        this.stockCount = value;
+    }
+
+    public short getYear() {
+        return this.year;
+    }
+
+    public void setYear(short value) {
+        this.year = value;
+    }
+
+    public String getId() {
+        return this.id;
+    }
+
+    public void setId(String value) {
+        this.id = value;
+    }
+
     public String getInformation() {
         return this.information;
+    }
+
+    public void setInformation(String value) {
+        this.information = value;
+    }
+
+    public String getManufacturer() {
+        return this.manufacturer;
+    }
+
+    public void setManufacturer(String value) {
+        this.manufacturer = value;
+    }
+
+    public String getName() {
+        return this.name;
+    }
+
+    public void setName(String value) {
+        this.name = value;
     }
     
     /**
